8cc30028df27b37a99ceac773718714ea1da4fe5
Jomo <johnisige2020@gmail.com>
2026-03-31 11:00:59 +0300

    nav links

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="index.css?v=<?php echo time(); ?>">
+  <title>NAVIGATION</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="header">
+
+      <div class="logo">
+        <h3>My App</h3>
+      </div>
+
+      <div class="nav">
+             <ul>
+              <li><a href="#">HOME</a></li>
+              <li><a href="#">ABOUT</a></li>
+              <li><a href="#">CONTACT</a></li>
+             </ul>
+      </div>
+
+    </div>
+  </div>
+</body>
+
+</html>
