9f239aa46514f50bb406e8ad4c5928aabb1e3a81
Jomo <johnisige2020@gmail.com>
2026-03-31 11:14:32 +0300

    your message

--- index.php
+++ index.php
@@ -10,22 +10,49 @@
 
 <body>
   <div class="container">
-    <div class="header">
+    <section>
+      <div class="header">
 
-      <div class="logo">
-        <h3>My App</h3>
+        <div class="logo">
+          <h3>My App</h3>
+        </div>
+
+        <div class="nav">
+          <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">CONTACT</a></li>
+          </ul>
+        </div>
+
       </div>
-
-      <div class="nav">
-             <ul>
-              <li><a href="#">HOME</a></li>
-              <li><a href="#">ABOUT</a></li>
-              <li><a href="#">CONTACT</a></li>
-             </ul>
-      </div>
-
-    </div>
   </div>
+  </section>
+
+  <div class="footer_section">
+    <footer>
+      <div class="footer_box">
+        <h2>John Doe</h2>
+        <p>Full Stack Developer</p>
+        <p>john.doe@example.com</p>
+      </div>
+
+      <div class="footer_box">
+        <h2>Skills</h2>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>JavaScript</p>
+      </div>
+
+      <div class="footer_box">
+        <h2>Quick Links</h2>
+        <p><a href="#">HOME</a></p>
+        <p><a href="#">ABOUT</a></p>
+        <p><a href="#">CONTACT</a></p>
+      </div>
+    </footer>
+  </div>
+
 </body>
 
 </html>
